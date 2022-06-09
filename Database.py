f9bf1a38b97673f946c29e6a046e90f06e058d60
Amitkor12 <amitcor12@gmail.com>
2022-06-09 17:09:04 +0530

    some more data into table

--- Database.py
+++ Database.py
@@ -7,7 +7,11 @@ connection = sqlite3.connect('customer.db')
 c = connection.cursor()
 
 # providing table content 
-c.execute(" INSERT INTO customers VALUES ('Amit','Korgoankar','amitkor@gmail.com')")
+c.execute(" INSERT INTO customers VALUES ('hemant','Korgoankar','hmu@gmail.com')")
+c.execute(" INSERT INTO customers VALUES ('anant','Korgoankar','anant@gmail.com')")
+
+
+print("command executed succesfully")
 
 # commiting our database
 connection.commit()
