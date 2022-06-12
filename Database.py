ac5e330ba6245ca3e097c3e8e0bf1b808fb023f6
Amitkor12 <amitcor12@gmail.com>
2022-06-12 11:49:25 +0530

    Using And & Or with Like, wildcard

--- Database.py
+++ Database.py
@@ -6,18 +6,9 @@ connection = sqlite3.connect('customer.db')
 # creat a cursor
 c = connection.cursor()
 
-# query the database -- ordered by
-# assending order
-c.execute("SELECT rowid, * FROM customers ORDER BY last_name ")
+# query the database -- And/Or
+c.execute("SELECT rowid, * FROM customers WHERE first_name like 'A%' AND last_name like 'Kor%'")
 
-# desending order
-# c.execute("SELECT rowid, * FROM customers ORDER BY last_name DESC")
-
-
-#fetchone()
-#fetchmany(3)
-# print("NAME " + " \t\t" + "EMAIL")
-# print("______"+"\t\t______")
 items = c.fetchall()
 for item in items:
     print(item)
