a898feb306ee7cc3adf423af24c05ddb81186990
Amitkor12 <amitcor12@gmail.com>
2022-06-15 12:12:34 +0530

    adiing multiple users

--- App.py
+++ App.py
@@ -1,5 +1,11 @@
 import Database
 
-#Database.add_records("Rohol","wuhyr","Rauh@whoatyou.com")
-Database.delet_records('10')
+# Database.add_records("Rohol","wuhyr","Rauh@whoatyou.com")
+# Database.delet_records('10')
+stuff = [
+    ('john', 'dsilva', 'dsilva@comedy.com'),
+    ('gandhi', 'shet', 'shet@codemy.com'),
+    ('holy', 'father', 'fatherco@yuo.com')
+    ]
+Database.multi_records(stuff)
 Database.sqldata()
--- Database.py
+++ Database.py
@@ -25,6 +25,15 @@ def add_records(first,last,email):
     connection.commit()
     connection.close()
 
+def multi_records(list):
+    connection = sqlite3.connect('customer.db')
+    curs = connection.cursor()
+
+    curs.executemany("insert into customers values(?,?,?)",(list))
+
+    connection.commit()
+    connection.close()    
+
 def delet_records(id):
     connection = sqlite3.connect('customer.db')
     curs = connection.cursor()
